f3aa86c7f477e7c5f98ef58bbcd8e45efe79130b
CuongNV <cuong@mission.plus>
2022-06-27 11:26:45 +0700

    test husky

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-let a = b;
+let a = b + c;
 app.listen(3000, () => {
   console.log("server running on port : " + PORT);
 });
