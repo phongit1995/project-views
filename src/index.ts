ea838a816a7ea7bc975e538de9fd7f6b97094a50
CuongNV <cuong@mission.plus>
2022-06-27 11:21:55 +0700

    init 3

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("Phong");
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+app.listen(3000, () => {
+  console.log("server running on port : " + PORT);
+});
