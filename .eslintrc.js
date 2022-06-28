401942f0cfdea064b1c34779d06649f37af5cf34
phongit <phongit1995@gmail.com>
2022-06-28 21:13:12 +0700

    update node

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,11 @@ module.exports = {
     "@typescript-eslint/explicit-module-boundary-types": "off",
     "@typescript-eslint/no-explicit-any": "off",
     "semi": ["error", "always"],
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
   },
 };
